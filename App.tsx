c89eb148b944453033572137b9a6666c22f889e1
Anna Sekscinska <annasekscinska03@gmail.com>
2026-03-03 23:55:03 +0100

    Add screens, add styles

--- App.tsx
+++ App.tsx
@@ -1,45 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {createStaticNavigation, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator, NativeStackNavigationProp} from '@react-navigation/native-stack';
+//import {Link} from '@react-navigation/native';
+//import {Button} from '@react-navigation/elements';
+import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+type RootStackParamList = {
+    Home: undefined;
+    Profile: undefined;
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+type NavigationProp = NativeStackNavigationProp<RootStackParamList,'Home'>;
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+
+const RootStack = createNativeStackNavigator<RootStackParamList>({
+    screens: {
+        Home: {
+            screen: AppScreen,
+            options: {title: 'Home'},
+        },
+        Profile: {
+            screen: ScanScreen,
+            options: {title: 'Profile'},
+        },
+    },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+    return <Navigation />
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+function AppScreen() {
+    const navigation = useNavigation<NavigationProp>();
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello World</Text>
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+            <TouchableOpacity style={styles.buttonScan} onPress={()=> navigation.navigate('Profile')}>
+                <Text style={styles.buttonScanText}>{"Scan!"}</Text>
+            </TouchableOpacity>
+        </View>
+
+    )
+}
+
+function ScanScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Scan your cosmetics!</Text>
+        </View>
+
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'pink',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'green',
+    },
+    buttonScan: {
+        margin: 20,
+        alignItems: 'center',
+        width: 100,
+        height: 40,
+        backgroundColor: 'green',
+        justifyContent: 'center',
+        borderRadius: 10,
+    },
+    buttonScanText: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: 'white',
 
-export default App;
+    }
+})
+
+//export default ScanScreen;
+//export default AppScreen;
